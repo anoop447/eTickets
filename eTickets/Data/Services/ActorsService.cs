c83f4f0c97b3808fe02745974a98fc0a064f0f03
Anoop Bhat <anoopbhat47@gmail.com>
2022-07-02 23:58:57 +0530

    new commits

---
+++ eTickets/Data/Services/ActorsService.cs
@@ -0,0 +1,45 @@
+﻿using eTickets.Models;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data.Services
+{
+    public class ActorsService : IActorsService
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsService(AppDbContext context)
+        {
+            _context = context;
+
+        }
+        public void Add(Actor actor)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Actor Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Actor>> GetAll()
+        {
+            var result = await _context.Actors.ToListAsync();
+            return result;
+        }
+
+        public Actor Update(int id, Actor newActor)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using eTickets.Models;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        Task<IEnumerable<Actor>> GetAll();
+
+        Actor Get(int id);
+        void Add(Actor actor);
+
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
