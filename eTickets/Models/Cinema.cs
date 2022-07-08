c8f4b30ebefda916fab874135e80105073edd1bd
Anoop Bhat <anoopbhat47@gmail.com>
2022-07-09 00:04:33 +0530

    more services

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -10,13 +10,18 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         [Display(Name ="Cinema Logo")]
+        [Required(ErrorMessage = "Cinema Logo is required")]
+
         public string Logo { get; set; }
 
         [Display(Name = "Cinema Name")]
+        [Required(ErrorMessage = "Cinema Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be in between 3 and 50 chars")]
 
         public string Name { get; set; }
 
         [Display(Name = "Description")]
+        [Required(ErrorMessage = "Description is required")]
 
         public string Description { get; set; }
 
