41a1ba0f04517a8908717544e911b70bde0b9293
Anoop Bhat <anoopbhat47@gmail.com>
2022-07-08 23:11:33 +0530

    new services

---
+++ eTickets/Data/Services/ICinemasService.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Text;
+using System;
+using eTickets.Data.Base;
+using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface ICinemasService: IEntityBaseRepository<Cinema>
+    {
+    }
+}
--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -1,21 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System;
 using System.Collections.Generic;
+using eTickets.Data.Base;
 
 namespace eTickets.Models
 {
-    public class Producer
+    public class Producer:IEntityBase
     {
         [Key]
         public int Id { get; set; }
 
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage ="Profile Picture is required")] 
         public string ProfilePictureURL { get; set; }
 
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage ="Full name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full name must be in between 3 and 50 chars")]
+
         public string FullName { get; set; }
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage ="Bio is required")]
 
         public string Bio { get; set; }
 
