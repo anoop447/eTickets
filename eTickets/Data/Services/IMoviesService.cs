ef9072be9ec0bd15ba5a81fbfc7b600e31dc7a64
Anoop Bhat <anoopbhat47@gmail.com>
2022-07-10 00:22:58 +0530

    partial views

--- eTickets/Data/Services/IMoviesService.cs
+++ eTickets/Data/Services/IMoviesService.cs
@@ -11,6 +11,7 @@ namespace eTickets.Data.Services
     {
         Task<Movie> GetMovieByIdAsync(int id);
         Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues();
+        Task AddNewMovieAsync(NewMovieVM data);
 
     }
 }
